5f33c6bea817ea64924aabd214f2c89c431e84f1
Giuliano Grasso <giuliano.grasso26@gmail.com>
2023-03-15 22:23:51 +0000

    add fade in animations to elements

---
+++ main.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const fadeIn = document.querySelectorAll('.fade-in')
+const fadeInLeft = document.querySelectorAll('.fade-in-left')
+const fadeInRight = document.querySelectorAll('.fade-in-right')
+fadeIn.forEach((el) => observer.observe(el))
+fadeInLeft.forEach((el) => observer.observe(el))
+fadeInRight.forEach((el) => observer.observe(el))
